2c06a6094a77ee7f03c43e269502d3ab6bad0135
kd1363656 <kd1363656@st.kobedenshi.ac.jp>
2026-04-28 11:44:43 +0900

    D3D12MAライブラリを使う(実装コストが高いと判断)

--- Source/Framework/Graphics/Resource/Heap/Allocator/Texture/TexturePlacedResourceAllocator.cpp
+++
@@ -1,6 +0,0 @@
-﻿#include "TexturePlacedResourceAllocator.h"
-
-FWK::Graphics::TexturePlacedResourceAllocator::TexturePlacedResourceAllocator() : 
-	PlacedResourceAllocatorBase(k_textureHeapType, k_textureHeapFlags)
-{}
-FWK::Graphics::TexturePlacedResourceAllocator::~TexturePlacedResourceAllocator() = default;
