f406f1ff5434b79fa4d5f9c267081569356b5b9d
kd1363656 <kd1363656@st.kobedenshi.ac.jp>
2026-05-03 14:26:26 +0900

    テクスチャクラスの作成及びTextureIDの管理手法の改善

---
+++ Source/Framework/Graphics/Resource/Texture/Texture.cpp
@@ -0,0 +1,10 @@
+﻿#include "Texture.h"
+
+void FWK::Graphics::Texture::Load(const std::filesystem::path& a_filePath)
+{
+	auto& l_graphicsManager = FWK::Graphics::GraphicsManager::GetInstance();
+
+	auto& l_resourceContext = l_graphicsManager.GetREFResourceContext();
+	auto& l_textureSystem   = l_resourceContext.GetREFTextureSystem  ();
+
+}
---
+++ Source/Framework/Graphics/Resource/Texture/Texture.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+namespace FWK::Graphics
+{
+	class Texture final
+	{
+	public:
+		
+		 Texture() = default;
+		~Texture() = default;
+
+		void Load(const std::filesystem::path& a_filePath);
+
+		auto GetVALTextureID() const { return m_textureID; }
+
+	private:
+
+		TypeAlias::TextureID m_textureID = Constant::k_invalidTextureID;
+	};
+}
