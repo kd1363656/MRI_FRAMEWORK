0f546107afc851cb59ccc0a78a62d3e1ab846f0d
kd1363656 <kd1363656@st.kobedenshi.ac.jp>
2026-05-06 13:28:59 +0900

    シェーダーを.csoだけで読み込めるようにした

--- Source/Framework/Graphics/Shader/Shader.cpp
+++ Source/Framework/Graphics/Shader/Shader.cpp
@@ -9,23 +9,7 @@ void FWK::Graphics::Shader::Deserialize(const nlohmann::json& a_rootJson)
 
 bool FWK::Graphics::Shader::CreateFromFile(const ShaderCompiler& a_shaderCompiler)
 {
-	if (m_filePath.extension() == Constant::k_lowerHLSLExtension)
-	{
-		if (m_entryPointName.empty())
-		{
-			assert(false && "HLSLシェーダーのエントリーポイント名が空のため、シェーダー作成に失敗しました。");
-			return false;
-		}
-
-		if (m_shaderModelVersionName.empty())
-		{
-			assert(false && "HLSLシェーダーモデルバージョン名が空のため、シェーダー作成に失敗しました。");
-			return false;
-		}
-
-		m_dxcBlob = a_shaderCompiler.CompileFromFile(m_filePath.c_str(), m_entryPointName, m_shaderModelVersionName);
-	}
-	else if (m_filePath.extension() == Constant::k_lowerCSOExtension)
+	if (m_filePath.extension() == Constant::k_lowerCSOExtension)
 	{
 		m_dxcBlob = a_shaderCompiler.LoadBinaryFromFile(m_filePath.c_str());
 	}
--- Source/Framework/Graphics/Shader/ShaderCompiler.h
+++ Source/Framework/Graphics/Shader/ShaderCompiler.h
@@ -14,25 +14,10 @@ namespace FWK::Graphics
 		// コンパイル対象のシェーダーを格納するIDxcBlobを引数にしているのは
 		// しっかりコンパイルできたかどうかを"bool"で確認するため
 		// HLSLファイルを読み込み、指定エントリーポイントとシェーダーモデルでコンパイルし、
-		// DXILバイトコードを出力Blobとして返す
-		TypeAlias::ComPtr<IDxcBlob> CompileFromFile   (const std::wstring& a_filePath, const std::string& a_entryPointName, const std::string& a_shaderModelVersionName) const;
+		// DXILバイトコードを出力Blobとして返
 		TypeAlias::ComPtr<IDxcBlob> LoadBinaryFromFile(const std::wstring& a_filePath) const;
 
 	private:
-
-		static constexpr const wchar_t* const k_argEntryPointOption    = L"-E";
-		static constexpr const wchar_t* const k_argTargetProfileOption = L"-T";
-		static constexpr const wchar_t* const k_argRowMajorMatrix      = L"-Zpr";
-		static constexpr const wchar_t* const k_argHLSLVersionOption   = L"-HV";
-		static constexpr const wchar_t* const k_argHLSLVersion         = L"2021";
-
-#if defined(_DEBUG)
-		static constexpr const wchar_t* const k_argDebugINFO      = L"-Zi";
-		static constexpr const wchar_t* const k_argEmbedDebugINFO = L"-Qembed_debug";
-#else
-		static constexpr const wchar_t* const k_argOptimizationLevel = L"-O3";
-#endif
-
 
 		TypeAlias::ComPtr<IDxcUtils>     m_dxcUtils    = nullptr;
 		TypeAlias::ComPtr<IDxcCompiler3> m_dxcCompiler = nullptr;
