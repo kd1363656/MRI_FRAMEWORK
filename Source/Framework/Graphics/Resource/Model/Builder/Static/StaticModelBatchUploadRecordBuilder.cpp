5b7320189da9524190b022885a9bffed606b2feb
kd1363656 <kd1363656@st.kobedenshi.ac.jp>
2026-05-17 10:55:58 +0900

    更新

---
+++ Source/Framework/Graphics/Resource/Model/Builder/Static/StaticModelBatchUploadRecordBuilder.cpp
@@ -0,0 +1,19 @@
+﻿#include "StaticModelBatchUploadRecordBuilder.h"
+
+bool FWK::Graphics::StaticModelBatchUploadRecordBuilder::CreateStaticModelBatchUploadRecord(const std::weak_ptr<Struct::StaticModelRecord>& a_staticModelRecord, 
+																							const Device&									a_device, 
+																						    const GPUMemoryAllocator&						a_gpuMemoryAllocator, 
+																								  std::vector<Struct::BufferUploadCommand>& a_bufferUploadCommandList,
+																								  DescriptorPool<SRVDescriptorHeap>&		a_srvDescriptorHeap) const
+{
+	return false;
+}
+
+bool FWK::Graphics::StaticModelBatchUploadRecordBuilder::CreateModelBatchUploadRecord(const Device&				                      a_device, 
+																					  const GPUMemoryAllocator&                       a_gpuMemoryAllocator, 
+																							std::vector<Struct::BufferUploadCommand>& a_bufferUploadCommmandList, 
+																							DescriptorPool<SRVDescriptorHeap>&		  a_srvDescriptorHeap, 
+																							Struct::ModelMesh&						  a_modelMesh)
+{
+	return false;
+}
--- Source/Framework/Graphics/Resource/Model/Builder/Static/StaticModelBatchUploadRecordBuilder.h
+++ Source/Framework/Graphics/Resource/Model/Builder/Static/StaticModelBatchUploadRecordBuilder.h
@@ -9,21 +9,39 @@ namespace FWK::Graphics
 		 StaticModelBatchUploadRecordBuilder() = default;
 		~StaticModelBatchUploadRecordBuilder() = default;
 
-		bool CreateStaticModelBatchUploadRecord(const Device&			                 a_device,
-												const GPUMemoryAllocator&                a_gpuMemoryAllocator,
-												const Struct::ModelData&                 a_modelData,
-													  DescriptorPool<SRVDescriptorHeap>& a_srvDescriptorPool,
-													  Struct::StaticModelRecord&		 a_staticModelRecord) const;
+		bool CreateStaticModelBatchUploadRecord(const std::weak_ptr<Struct::StaticModelRecord>& a_staticModelRecord,
+												const Device&									a_device,
+												const GPUMemoryAllocator&					    a_gpuMemoryAllocator,
+													  std::vector<Struct::BufferUploadCommand>& a_bufferUploadCommandList,
+													  DescriptorPool<SRVDescriptorHeap>&		a_srvDescriptorHeap) const;
 
 	private:
 
 		static constexpr UINT64 k_firstStructuredBufferElement = 0ULL;
 
-		bool CreateModelMeshBatchUploadRecord(const Device&									  a_device,
-											  const GPUMemoryAllocator&						  a_gpuMemoryAllocator,
-											  const Struct::ModelMesh&						  a_modelMesh,
-													std::vector<Struct::BufferUploadCommand>& a_bufferUploadCommandList,
-													DescriptorPool<SRVDescriptorHeap>&        a_srvDescriptorPool,
-												    Struct::ModelMeshRuntimeData&             a_modelMeshRuntimeData);
+		bool CreateModelBatchUploadRecord(const Device&									  a_device,
+										  const GPUMemoryAllocator&						  a_gpuMemoryAllocator,
+											    std::vector<Struct::BufferUploadCommand>& a_bufferUploadCommmandList,
+												DescriptorPool<SRVDescriptorHeap>&		  a_srvDescriptorHeap,
+												Struct::ModelMesh&						  a_modelMesh);
+
+		template <typename Type>
+		bool CreateBufferUploadCommand(const std::vector<Type>&                        a_bufferList,
+									   const Device&			                       a_device,
+									   const GPUMemoryAllocator&                       a_gpuMemoryAllocator,
+										     std::vector<Struct::BufferUploadCommand>& a_bufferUploadCommandList,
+											 TypeAlias::ComPtr<ID3D12Resource2>&       a_destinationBufferResource,
+											 TypeAlias::ComPtr<D3D12MA::Allocation>&   a_destinationBufferAllocation) const
+		{
+			if (a_bufferList.empty()) 
+			{
+				assert(false && "BufferListが空のため、BufferUploadCommandの作成に失敗しました。");
+				return false;
+			}
+
+			const auto& l_bufferSize = sizeof(Type) * a_bufferList.size();
+
+
+		}
 	};
 }
