67ddc1a2ab4413e08113f88edd713711a37aad6c
kd1363656 <kd1363656@st.kobedenshi.ac.jp>
2026-05-30 17:30:56 +0900

    CommandListBaeまで完了

--- Source/Framework/Graphics/Command/Allocator/CommandAllocatorBase.cpp
+++ Source/Framework/Graphics/Command/Allocator/CommandAllocatorBase.cpp
@@ -14,38 +14,25 @@ bool FWK::Graphics::CommandAllocatorBase::Create(const Device& a_device)
 	const auto& l_device = a_device.GetREFDevice();
 
 	// デバイスが存在しないなら作成できないのでreturn
-	if (!l_device)
-	{
-		assert(false && "デバイスの作成に失敗しており、コマンドアロケータの作成が出来ませんでした。");
-		return false;
-	}
+	FWK_ASSERT_RETURN_VALUE_IF(!l_device, "デバイスの作成に失敗しており、コマンドアロケータの作成が出来ませんでした。", false)
+
 
 	// コマンドアロケータを作成する関数
 	// CreateCommandAllocator(このコマンドアロケータが記録対象とするコマンドリストの種類(※注意 : 作成するコマンドリストの種類と合わせる必要がある)、
 	//						  受け取りたいCOMインターフェース型のID、
 	//					      作成結果のポインタを書き込むアドレス);
-
 	auto l_hr = l_device->CreateCommandAllocator(k_createCommandListType, IID_PPV_ARGS(m_commandAllocator.ReleaseAndGetAddressOf()));
 
-	if (FAILED(l_hr))
-	{
-		assert(false && "コマンドアロケータの作成に失敗しました。");
-		return false;
-	}
+	FWK_ASSERT_RETURN_VALUE_IF(FAILED(l_hr), "コマンドアロケータの作成に失敗しました。", false)
 
 	return true;
 }
 
 void FWK::Graphics::CommandAllocatorBase::Reset() const
 {
-	if (!m_commandAllocator)
-	{
-		assert(false && "コマンドアロケータの作成に失敗しており、リセット処理を行うことができませんでした。");
-		return;
-	}
+	FWK_ASSERT_RETURN_IF(!m_commandAllocator, "コマンドアロケータの作成に失敗しており、リセット処理を行うことができませんでした。")
 
 	// コマンドアロケータを再利用できる状態に戻す関数
 	// Reset();
-
 	m_commandAllocator->Reset();
 }
--- Source/Framework/Graphics/Command/List/CommandListBase.cpp
+++ Source/Framework/Graphics/Command/List/CommandListBase.cpp
@@ -11,11 +11,7 @@ bool FWK::Graphics::CommandListBase::Create(const Device& a_device)
 {
 	const auto& l_device = a_device.GetREFDevice();
 
-	if (!l_device)
-	{
-		assert(false && "デバイスの作成に失敗しており、コマンドリストの作成に失敗しました。");
-		return false;
-	}
+	FWK_ASSERT_RETURN_VALUE_IF(!l_device, "デバイスの作成に失敗しており、コマンドリストの作成に失敗しました。", false)
 
 	// 閉じた状態のコマンドリストを作成する関数(命令を書き込む前にResetして記録可能状態にする必要がある)
 	// CreateCommandList(このコマンドリストを作成するGPUノード指定値、
@@ -23,62 +19,40 @@ bool FWK::Graphics::CommandListBase::Create(const Device& a_device)
 	//					 コマンドリスト作成時の追加オプション、
 	//					 受け取りたいCOMインターフェース型のID、
 	//					 作成結果のポインタを書き込むアドレス);
-
 	auto l_hr = l_device->CreateCommandList1(Constant::k_defaultGPUNodeMask,
 											 k_createCommandListType,
 											 D3D12_COMMAND_LIST_FLAG_NONE,
 											 IID_PPV_ARGS(m_commandList.ReleaseAndGetAddressOf()));
 
-	if (FAILED(l_hr))
-	{
-		assert(false && "コマンドリストの作成に失敗しました。");
-		return false;
-	}
+	FWK_ASSERT_RETURN_VALUE_IF(FAILED(l_hr), "コマンドリストの作成に失敗しました。", false)
 
 	return true;
 }
 
 void FWK::Graphics::CommandListBase::Reset(const CommandAllocatorBase& a_commandAllocator)
 {
-	if (!m_commandList)
-	{
-		assert(false && "コマンドリストの作成に失敗しており、コマンドリストのリセットが出来ませんでした。");
-		return;
-	}
+	FWK_ASSERT_RETURN_IF(!m_commandList, "コマンドリストの作成に失敗しており、コマンドリストのリセットが出来ませんでした。")
 
 	// 自身のコマンドリストタイプと一致しなければreturn
-	if (a_commandAllocator.GetVALCreateCommandListType() != k_createCommandListType)
-	{
-		assert(false && "コマンドアロケータのコマンドリストタイプと一致しないため、コマンドリストのリセットが出来ませんでした。");
-		return;
-	}
+	FWK_ASSERT_RETURN_IF(a_commandAllocator.GetVALCreateCommandListType() != k_createCommandListType, "コマンドアロケータのコマンドリストタイプと一致しないため、コマンドリストのリセットが出来ませんでした。")
 
 	const auto& l_d3dCommandAllocator = a_commandAllocator.GetREFCommandAllocator();
 
-	if (!l_d3dCommandAllocator)
-	{
-		assert(false && "コマンドアロケータの作成に失敗しており、コマンドリストのリセットに失敗しました。");
-		return;
-	}
+	FWK_ASSERT_RETURN_IF(!l_d3dCommandAllocator, "コマンドアロケータの作成に失敗しており、コマンドリストのリセットに失敗しました。")
 
 	// コマンドリストを再び記録できる状態に戻す関数
 	// Reset(使用していたコマンドアロケータ、
 	//		 最初に設定するパイプラインステート);
-
 	m_commandList->Reset(l_d3dCommandAllocator.Get(), nullptr);
 }
 
 void FWK::Graphics::CommandListBase::Close() const
 {
-	if (!m_commandList)
-	{
-		assert(false && "コマンドリストの作成に失敗しており、コマンドリストのクローズ処理が出来ませんでした。");
-		return;
-	}
+	FWK_ASSERT_RETURN_IF(!m_commandList, "コマンドリストの作成に失敗しており、コマンドリストのクローズ処理が出来ませんでした。")
+
 
 	// コマンドリストへの命令記録を終了するクラス
 	// ※注意 : もしCloseをしなければコマンドキューのExecute処理を行うことができない
 	// Close();
-
 	m_commandList->Close();
 }
