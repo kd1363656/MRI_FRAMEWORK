932f5ac80c301752da1ed836996237d5b6f8630f
kd1363656 <kd1363656@st.kobedenshi.ac.jp>
2026-05-31 20:46:54 +0900

    DXCコンパイラまでリファクタリング完了

--- Source/Framework/Graphics/Shader/ShaderCompiler.cpp
+++ Source/Framework/Graphics/Shader/ShaderCompiler.cpp
@@ -2,43 +2,30 @@
 
 bool FWK::Graphics::ShaderCompiler::Create()
 {
+	// 今回はDxcUtilsを作成している。
+	// DxcUtilsはファイル読み込みや文字コード変換、include処理補助など、
+	// DXCを使う周辺処理で使う便利機能用オブジェクト
 	// DXC(DX Shader Compiler)関連のCOMオブジェクト作成する関数
 	// DxcCreateInstance(何を作るのかを表すCLSID、
 	//					 受け取りたいCOMインターフェース型のID、
 	//					 作成結果のポインタを書き込むアドレス);
-
-	// 今回はDxcUtilsを作成している。
-	// DxcUtilsはファイル読み込みや文字コード変換、include処理補助など、
-	// DXCを使う周辺処理で使う便利機能用オブジェクト
 	auto l_hr = DxcCreateInstance(CLSID_DxcUtils, IID_PPV_ARGS(m_dxcUtils.ReleaseAndGetAddressOf()));
 
-	if (FAILED(l_hr))
-	{
-		assert(false && "DxcUtilsの作成に失敗しました。");
-		return false;
-	}
+	FWK_ASSERT_RETURN_VALUE_IF(FAILED(l_hr), "DxcUtilsの作成に失敗しました。", false)
 
 	// DXC本体のコンパイラオブジェクトを作成する
 	// CLSID_DxcCompilerは「シェーダーをコンパイルする本体」を表す識別子。
 	// これを作成することで、HLSLコードをDXILへコンパイルできるようになる。
 	l_hr = DxcCreateInstance(CLSID_DxcCompiler, IID_PPV_ARGS(m_dxcCompiler.ReleaseAndGetAddressOf()));
 
-	if (FAILED(l_hr))
-	{
-		assert(false && "DxcCompilerの作成に失敗しました。");
-		return false;
-	}
+	FWK_ASSERT_RETURN_VALUE_IF(FAILED(l_hr), "DxcCompilerの作成に失敗しました。", false)
 
 	return true;
 }
 
 FWK::TypeAlias::ComPtr<IDxcBlob> FWK::Graphics::ShaderCompiler::LoadBinaryFromFile(const std::wstring& a_filePath) const
 {
-	if (!m_dxcUtils)
-	{
-		assert(false && "DxcUtilsの作成がされておらず、シェーダーバイナリファイルの読み込みができませんでした。");
-		return nullptr;
-	}
+	FWK_ASSERT_RETURN_VALUE_IF(!m_dxcUtils, "DxcUtilsの作成がされておらず、シェーダーバイナリファイルの読み込みができませんでした。", nullptr)
 
 	if (a_filePath.empty())
 	{
@@ -69,11 +56,7 @@ FWK::TypeAlias::ComPtr<IDxcBlob> FWK::Graphics::ShaderCompiler::LoadBinaryFromFi
 	// PSO作成で使うD3D12_SHADER_BYTECODE用のIDxcBlobとして扱えるように変換する。
 	l_hr = l_binaryBlob.As(&l_dxcBlob);
 
-	if (FAILED(l_hr))
-	{
-		assert(false && "IDxcBlobEncodingからIDxcBlobへの変換に失敗しました。");
-		return nullptr;
-	}
+	FWK_ASSERT_RETURN_VALUE_IF(FAILED(l_hr), "IDxcBlobEncodingからIDxcBlobへの変換に失敗しました。", nullptr)
 
 	return l_dxcBlob;
 }
