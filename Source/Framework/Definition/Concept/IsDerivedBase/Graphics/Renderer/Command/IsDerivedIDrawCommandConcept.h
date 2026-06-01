26c2562c11e9073f50a8ab36960f20c9f99446b2
kd1363656 <kd1363656@st.kobedenshi.ac.jp>
2026-06-01 17:09:27 +0900

    DrawCommand関係の余計なフィルガターを削除

--- Source/Framework/Definition/Concept/IsDerivedBase/Graphics/Renderer/Command/IsDerivedIDrawCommandConcept.h
+++
@@ -1,8 +0,0 @@
-﻿#pragma once
-
-namespace FWK::Concept
-{
-	// DrawCommandBaseを継承したクラスかどうかを確認するコンセプト
-	template <typename Type>
-	concept IsDerivedDrawCommandBaseConcept = IsDerivedBaseConcept<Type, Graphics::DrawCommandBase>;
-}
--- Source/Framework/Definition/Type/Alias/Factory/Shared/Graphics/Render/Command/TypeAliasShaderFactoryDrawCommand.h
+++
@@ -1,6 +0,0 @@
-﻿#pragma once
-
-namespace FWK::TypeAlias
-{
-	using ShaderFactoryDrawCommand = GenericFactory<std::shared_ptr<Graphics::DrawCommandBase>>;
-}
