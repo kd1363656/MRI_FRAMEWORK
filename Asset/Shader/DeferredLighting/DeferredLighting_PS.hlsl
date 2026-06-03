712a2bce18a7aefa2a9e3d3589fccdaa0b7cddd7
kd1363656 <kd1363656@st.kobedenshi.ac.jp>
2026-06-03 10:06:33 +0900

    ディファード、フォアード、マルチパスレンダリングが一応完成

--- Asset/Shader/DeferredLighting/DeferredLighting_PS.hlsl
+++ Asset/Shader/DeferredLighting/DeferredLighting_PS.hlsl
@@ -1,8 +1,19 @@
 ﻿#include "DeferredLighting.hlsli"
 
+static const float k_deferredLightingInvalidWorldPositionW = 0.0F;
+static const float4 k_deferredLightingDebugBackgroundColor = float4(1.0F, 1.0F, 1.0F, 1.0F);
+
 float4 main(MeshOutput a_input) : SV_Target0
 {
-    Texture2D<float4> l_gBufferAlbedoTexture = ResourceDescriptorHeap[g_gBufferAlbedoTextureSRVIndex];
+    Texture2D<float4> l_gBufferAlbedoTexture        = ResourceDescriptorHeap[g_gBufferAlbedoTextureSRVIndex];
+    Texture2D<float4> l_gBufferWorldPositionTexture = ResourceDescriptorHeap[g_gBufferWorldPositionTextureSRVIndex];
+    
+    const float4 l_gBufferWorldPosition = l_gBufferWorldPositionTexture.Sample(g_deferredLightingSampler, a_input.uv);
+    
+    if (l_gBufferWorldPosition.w <= k_deferredLightingInvalidWorldPositionW)
+    {
+        discard;
+    }
     
     return l_gBufferAlbedoTexture.Sample(g_deferredLightingSampler, a_input.uv);
 }
